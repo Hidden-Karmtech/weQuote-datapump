2f396211d95aa71a9edfe473153750a9de19b892
unknown <a.candelari@nb-candelari.apra.it>
2014-12-01 14:16:30 +0100

    primo

---
+++ google-api.js
@@ -0,0 +1,19 @@
+var http = require('request'),
+    googleSpreadsheet = require("google-spreadsheet"),
+    config = require("./config");
+var Q = require('q');
+
+module.exports = {
+	getDataTable : function(id)
+	{
+	    var deferred = Q.defer();
+		var mySheet = new googleSpreadsheet(id);
+		mySheet.setAuth(config.googleUsername,config.googlePassword, function(err){
+			mySheet.getRows(config.outputPageNumber ,function(err, rowData){
+				console.log(rowData.length);
+				deferred.resolve(rowData);
+			});
+		});
+		return deferred.promise;
+	}
+};
---
+++ wequote-api.js
@@ -0,0 +1,49 @@
+
+var config = require("./config");
+var apiUrl = config.apiUrl;
+var http = require('request');
+var Q = require('q');
+
+module.exports = {
+	list : function(params){
+	    var deferred = Q.defer();
+		http.get(
+			{
+				url: apiUrl + "/list",
+				qs: {
+					search: params.search 
+				},
+				json:true
+			},
+			function(error, response, result){
+				if(error){
+					deferred.reject(error);
+				}else{
+					deferred.resolve(result);
+				}
+			}
+			
+		);
+		return deferred.promise;
+	},
+	insert : function(quote){
+	    var deferred = Q.defer();
+		http.post(
+			{
+				url: apiUrl + "/insert",
+				form: {
+					quote: quote 
+				}
+			},
+			function(error, response, result){
+				if(error){
+					deferred.reject(error);
+				}else{
+					deferred.resolve(result);
+				}
+			}
+			
+		);
+		return deferred.promise;
+	}
+};
