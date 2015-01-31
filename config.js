7cc9cc5dc13748e3348c40426fca05cfc4c94703
Lillini Christian <c.lillini@gmail.com>
2015-01-31 11:37:37 +0100

    Utilizzo delle credenziali di accesso Google come variabili di ambiente

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 		outputId : "1at27VpZwZWtyrh2X5UznAAu3O_daR6KCVxWl0jyf0P4",
 		indexId : "1Dj7g_LAUu91xYY_0r2I0SevPUexXH6nRijQFQpRp2jk",
-		googleUsername : "**********",
-		googlePassword : "**********",
+		googleUsername : process.env.GOOGLE_USERNAME,
+		googlePassword : process.env.GOOGLE_PASSWORD,
 		outputPageNumber : 1,
 		apiUrl : "https://api-wequote.rhcloud.com"
 };
